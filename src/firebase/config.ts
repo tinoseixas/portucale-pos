e4c9987b722fa8dcdaa218f3a6f68d1bab1477e0
TS Serveis <eg.ad.tecnica@gmail.com>
2025-11-15 08:53:19 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1270748275-5ce91",
+  "appId": "1:548407285062:web:f73090ae428f24f3f34147",
+  "apiKey": "AIzaSyC6x6fy3q1IyP-zPfAmq7NILdiBXxub9FM",
+  "authDomain": "studio-1270748275-5ce91.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "548407285062"
+};
