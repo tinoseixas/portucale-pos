2294aa30cbda260175d7e0ed9691493fd4356190
TS Serveis <eg.ad.tecnica@gmail.com>
2025-11-15 10:04:58 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // Add the development origin to the list of allowed origins.
+    // This is necessary to allow cross-origin requests from the development environment.
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
