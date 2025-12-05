2b02068fd0418fc202d2f8753d148e23c41debfe
TS Serveis <eg.ad.tecnica@gmail.com>
2025-12-05 12:15:37 +0000

    Try fixing this error: `Console Error: [2025-12-05T12:13:23.067Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:548407285062:web:f73090ae428f24f3f34147",
   "apiKey": "AIzaSyC6x6fy3q1IyP-zPfAmq7NILdiBXxub9FM",
   "authDomain": "studio-1270748275-5ce91.firebaseapp.com",
+  "storageBucket": "studio-1270748275-5ce91.appspot.com",
   "measurementId": "",
   "messagingSenderId": "548407285062"
 };
